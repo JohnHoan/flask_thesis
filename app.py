17428c5eef51c1fa2a01bfd22be63a54b8d8f3d3
JohnHoan <hoan.nguyenjohn2004@hcmut.edu.vn>
2022-05-26 20:11:43 +0700

    ok

--- app.py
+++ app.py
@@ -1,7 +1,36 @@
-from turtle import speed
 from flask import Flask, request, jsonify
 app = Flask(__name__)
-import utils
+import pickle
+import json
+import numpy as np
+
+__data_columns = None
+__model = None
+def get_estimated_package(score_1,package_1,score_2,package_2,score_3,package_3,speed,satisfied_level):
+ 
+    x = np.zeros(len(__data_columns))
+    x[0] = score_1
+    x[1] = package_1
+    x[2] = score_2
+    x[3] = package_2
+    x[4] = score_3
+    x[5] = package_3
+    x[6] = speed
+    x[7] = satisfied_level
+    return str(__model.predict([x])[0])
+
+
+def load_saved_artifacts():
+    global  __data_columns
+    if __data_columns is None:
+        with open("columns.json", "r") as f:
+            __data_columns = json.load(f)['data_columns']
+
+    global __model
+    if __model is None:
+        with open('thesis.pickle', 'rb') as f:
+            __model = pickle.load(f)
+
 
 @app.route('/predict')
 def predict():
@@ -13,10 +42,9 @@ def predict():
     package_3 = float(request.values.get('package_3'))
     speed = float(request.values.get('speed'))
     satisfied_level = float(request.values.get('satisfied_level'))
-    output = utils.get_estimated_package(score_1,package_1,score_2,package_2,score_3,package_3,speed,satisfied_level)
+    output = get_estimated_package(score_1,package_1,score_2,package_2,score_3,package_3,speed,satisfied_level)
     return jsonify([output])
     
 if __name__ == "__main__":
-    print("Starting Python Flask Server...")
-    utils.load_saved_artifacts()
-    app.run()
+    load_saved_artifacts()
+    app.run(debug=True)
